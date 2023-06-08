33c06a738a6a900f0fce2d83738ed6fd96d77cf3
fitz95 <fitzgeraldnsong@gmail.com>
2023-06-08 14:50:36 +0100

    fix linter issues

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
-    def random
-        @greeting = Greeting.order("RANDOM()").first
-        render json: { greeting: @greeting }        
-    end
+  def random
+    @greeting = Greeting.order('RANDOM()').first
+    render json: { greeting: @greeting }
+  end
 end
